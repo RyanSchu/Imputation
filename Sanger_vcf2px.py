faa1ab80c3c592b339f9a75ff4c77c61979b50fa
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2018-10-31 14:42:12 -0500

    Update Sanger_vcf2px.py

--- Sanger_vcf2px.py
+++ Sanger_vcf2px.py
@@ -67,7 +67,7 @@ for line in gzip.open(chrfile):
     arr = line.strip().split()
     if(line.startswith('#CHROM')): #only one line should match #CHROM
         ids = arr[9:]
-	outdosage.write("chr snp_ID pos ref alt " + " ".join(ids) + '\n')
+	outdosage.write("chr snp_ID pos ref alt AA_freq " + " ".join(ids) + '\n')
         #split and join ids into FID and IID for PrediXcan
         ids2 = map(lambda x : x.split("_"), ids)
         ids = map(lambda x : ' '.join(x), ids2)
